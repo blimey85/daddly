8aff02eadb9850567e0257ee14ec6b453cf00c22
Gary Traffanstedt <blimey85@gmail.com>
2017-08-23 01:38:36 -0400

    Updated default_url for production email.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,7 @@ Rails.application.configure do
   config.action_mailer.sendgrid_actionmailer_settings = {
       api_key: ENV['SENDGRID_API_KEY']
   }
+  config.action_mailer.default_url_options = { host: 'daddly.io' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
