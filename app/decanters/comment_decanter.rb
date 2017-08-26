600fb4af1ca70591031c9dfe0353f804d24f3d61
Gary Traffanstedt <blimey85@gmail.com>
2017-08-26 00:55:04 -0400

    Re-enabled Decanter which after bug was resolved in latest version.

--- app/decanters/comment_decanter.rb
+++ app/decanters/comment_decanter.rb
@@ -9,14 +9,14 @@
 #  votes_count     :integer
 #
 
-# class CommentDecanter < Decanter::Base
-#   ignore :profile_picture_url, :fullname, :upvote_count, :created_at, :created_by_current_user, :user_has_upvoted
-#
-#   input :commentable_id, :integer
-#   input :commentable_type, :string
-#   input :body, :text
-#   input :parent_id, :integer
-#   input :user_id, :integer
-#   input :edited_at, :datetime
-#   input :votes_count, :integer
-# end
+class CommentDecanter < Decanter::Base
+  ignore :profile_picture_url, :fullname, :upvote_count, :created_at, :created_by_current_user, :user_has_upvoted
+
+  input :commentable_id, :integer
+  input :commentable_type, :string
+  input :body, :text
+  input :parent_id, :integer
+  input :user_id, :integer
+  input :edited_at, :datetime
+  input :votes_count, :integer
+end
--- lib/decanter/parsers/squash_date_time_parser.rb
+++ lib/decanter/parsers/squash_date_time_parser.rb
@@ -1,7 +1,7 @@
-# class SquashDateTimeParser < Decanter::Parser::ValueParser
-#   parser do |value|
-#     date, time = value.map(&:to_s)
-#     datetime = date + ' ' + time
-#     datetime.to_datetime
-#   end
-# end
+class SquashDateTimeParser < Decanter::Parser::ValueParser
+  parser do |value|
+    date, time = value.map(&:to_s)
+    datetime = date + ' ' + time
+    datetime.to_datetime
+  end
+end
